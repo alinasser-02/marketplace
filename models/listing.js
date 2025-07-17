c15311d52fd522aef7fd537f2db26bf50d2ec7bb
Ali Naser <3linassera7med@gmail.com>
2025-07-17 15:45:27 +0300

    comments section

--- models/listing.js
+++ models/listing.js
@@ -1,15 +1,30 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const listingSchema = new mongoose.Schema({
+const commentSchema = new mongoose.Schema(
+  {
+    content: String,
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+const listingSchema = new mongoose.Schema(
+  {
     title: String,
     description: String,
-    price: Number, 
+    price: Number,
     image: String,
-    seller: { 
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+    seller: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-}, { timestamps: true })
+    comments: [commentSchema],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Listing', listingSchema)
+module.exports = mongoose.model("Listing", listingSchema);
+
